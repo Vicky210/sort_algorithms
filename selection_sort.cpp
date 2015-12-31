b896bee01e8162a41454f0aee6d04a8d330b8469
Vicky210 <vigneshgm210@gmail.com>
2015-12-30 22:35:21 -0500

    Selection sort

---
+++ selection_sort.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conio.h>
+void selection_sort(int A[],int n){
+	int min,min_index,i,j; 
+	for(i=0;i<n;i++){
+		min=A[i];
+		min_index=i;
+		for(j=i;j<n;j++)
+			if(A[j]<min){
+				min = A[j];
+				min_index = j;
+			}
+		A[min_index]=A[i];
+		A[i]=min;
+	}
+}
+void main(){
+}
