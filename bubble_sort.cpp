ddbdd6cfdd6c1c4717a163741baebb1b25f6b63b
Vicky210 <vigneshgm210@gmail.com>
2015-12-30 22:56:28 -0500

    Bubble sort

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -3,7 +3,7 @@
 void bubble_sort(int A[],int n){
 	for(i=0;i<length;i++){
 		int flag =0;
-		for(j=0;j<(length-i-1);j++)
+		for(j=0;j<(length-i);j++)
 			if(a[j]>a[j++]){
 				int temp=a[j];
 				a[j]=a[j++];
