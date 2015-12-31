173bb3a056f4d0ccaf6e8a79a7a611dede8aa14c
Vicky210 <vigneshgm210@gmail.com>
2015-12-31 18:59:53 -0500

    Quick Sort

--- Quick_sort.cpp
+++ Quick_sort.cpp
@@ -6,14 +6,21 @@ void QuickSort(int A[],int start,int end){
 	QuickSort(A,start,p_index-1);
 	QuickSort(A,p_index+1,end);
 }
-void Partition(int A[],start,end){
+int Partition(int A[],start,end){
 	int pivot = A[end];
 	int p_index = start;
+	int temp;
 	for(i=0;i<n,i++)
 		if(A[i]<=pivot){
-			swap(A[i],A[p_index]);
+			temp = A[i];
+			A[i]=A[p_index];
+			A[p_index]=temp;
 			p_index++;
 		}
+	temp = A[p_index];
+	A[p_index]=A[end];
+	A[end]=temp;
+	return p_index;
 }
 int main(){
 	return 0;
