1d8f16517e6d856e3c8782dbb755f527effb6e71
Vicky210 <vigneshgm210@gmail.com>
2015-12-30 22:54:33 -0500

    Bubble sort

---
+++ bubble_sort.cpp
@@ -0,0 +1,19 @@
+#include<iostream.h>
+#include<conio.h>
+void bubble_sort(int A[],int n){
+	for(i=0;i<length;i++){
+		int flag =0;
+		for(j=0;j<(length-i-1);j++)
+			if(a[j]>a[j++]){
+				int temp=a[j];
+				a[j]=a[j++];
+				a[j++]=temp;
+				flag =1;
+			}
+		if(flag == 0)
+			break;
+	}
+}
+void main(){
+}	
+
