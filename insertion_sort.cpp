61797b03faf8ac54543c7d8e96877a85bd34d8a9
Vicky210 <vigneshgm210@gmail.com>
2016-02-16 13:01:48 -0500

    Update insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 void Insertion_sort(int A[],int n){
 	int i;
-	for(i=0;i<n;i++){
+	for(i=1;i<n;i++){
 		int current = A[i];
 		int current_index = i;
 		while(current_index>0 && A[current_index--]>A[current_index]){
